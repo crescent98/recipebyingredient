9a1f18e9c046d47a2dceb4207dec1a985c03b5ef
crescent98 <lego0142@naver.com>
2019-12-12 23:59:09 +0100

    first commit

---
+++ AwesomeProject/components/HomeScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+    render() {
+        return (
+          <View style={{alignItems: 'center'}}>
+            <Text>Hello!</Text>
+          </View>
+        );
+      }
+}
+  
